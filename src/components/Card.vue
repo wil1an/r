71fd39b559dd83e7f7ae9f0490d2a2f6d547947e
wil1an <tamichegwilliam@gmail.com>
2024-03-23 11:52:25 -0400

    p

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<script setup> 
+import { defineProps } from 'vue';
+    const props = defineProps({
+        Character: []
+    })
+</script>
+<template>
+    <div>
+        <div>
+            <img src="" alt="">
+        </div>
+        <div>
+            <h3>nombre</h3>
+        </div>
+    </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue';
+import Card from "../components/Card.vue"
+let character = ref([])
+
+
+async function loadCharacters(){
+  const respons = await fetch(`https://rickandmortyapi.com/api/character?page=1`)
+  const data = await ReportingObserver.json()
+  character.value = data.result
+  console.log(data)
+}
+
+</script>
+
+<template>
+  <div class="flex justify-center mt-10">
+    <h1 class="text-6xl text-red-500"> personakes de rick and morty </h1>
+  </div>
+  <div v-for="personajes in character" :key="character.id">
+     {{  personajes.name }} 
+  </div>
+  <!--<Card :character="character"/>-->
+</template>
